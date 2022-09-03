0189f9eee38b9986c3241ab254ccec2816189f4b
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2022-09-03 23:55:48 +0530

    start adding react router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -19,7 +19,7 @@ const Product = ({ product }) => {
                 <Card.Text as="div">
                     <Rating
                         value={product.rating}
-                        text={`${product.numReviews} reviews`}
+                        text={`${product.numReviews} reviews`}  
                     />
                 </Card.Text>
 
