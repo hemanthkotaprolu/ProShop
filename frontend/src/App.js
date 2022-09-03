93f6f478b12817308a39ca080a61a48c00d9718b
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2022-09-03 23:29:42 +0530

    started rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
     return (
         <>
-            <h1>Welcome to proshop</h1>
+            <Header />
+            <main>
+                <Container>
+                    <HomeScreen />
+                </Container>
+            </main>
+            <Footer />
         </>
     );
 };
