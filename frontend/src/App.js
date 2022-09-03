fc7fbb589aac271d58e5487bc01706559fd27cc2
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2022-09-04 00:20:47 +0530

    started ProductScreen implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
     return (
-        <>
+        <Router>
             <Header />
             <main>
                 <Container>
-                    <HomeScreen />
+                    <Routes>
+                        <Route path="/" element={<HomeScreen />} exact />
+                        <Route
+                            path="/product/:id"
+                            element={<ProductScreen />}
+                            exact
+                        />
+                    </Routes>
                 </Container>
             </main>
             <Footer />
-        </>
+        </Router>
     );
 };
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,7 +6,7 @@ import products from '../products';
 const HomeScreen = () => {
     return (
         <>
-            <h1>Latest Products</h1>
+            <h1 style={{ marginTop: '20px' }}>Latest Products</h1>
             <Row>
                 {products.map((product) => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={4}>
