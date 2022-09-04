df4df9a241a78bf254616e76ed41a67ad4d76d2b
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2022-09-04 19:56:03 +0530

    connect frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.send(product);
+});
+
+app.listen(port, console.log('Server started running on: ' + port));
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const ProductScreen = () => {
     const params = useParams();
-    const product = products.find((p) => p._id === params.id);
+    // const product = products.find((p) => p._id === params.id);
+
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${params.id}`);
+
+            setProduct(data);
+        };
+
+        fetchProduct();
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <>
             <Link className="btn btn-light my-3" to="/">
@@ -73,4 +87,4 @@ const ProductScreen = () => {
     );
 };
 
-export default ProductScreen
+export default ProductScreen;
